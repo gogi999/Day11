4fb2293b6d2f7bd26f7ad80be3f2557944c62d8c
gogi999 <g.petrovic9@gmail>
2021-03-18 16:40:39 +0100

    Added ex05/src/index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+var myNestedArray = [["Toblerone", 5]];
+
+function myNestedFunction(arr) {
+    arr.push(["Milka", 3]);
+    return arr;
+}
+// Only change code above this line
+console.log(myNestedFunction(myNestedArray)); // Change this line
+module.exports = myNestedFunction;
